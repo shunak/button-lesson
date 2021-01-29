ec1d95f7603444a6b09ffea6c7f005c1d1d752b6
shunak <shuna966@gmail.com>
2021-01-29 18:19:22 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LinePlot from "./components/LinePlot";
+import PiePlot from "./components/PiePlot";
+import BarPlot from "./components/BarPlot";
+import RadarPlot from "./components/RadarPlot";
+import BubblePlot from "./components/BubblePlot";
+
+function App() {
+  return (
+    <div className="App">
+      <LinePlot />
+      <PiePlot />
+      <BarPlot />
+      <RadarPlot />
+      <BubblePlot />
+    </div>
+  );
+}
+
+export default App;
